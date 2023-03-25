dcc21b006b62fa08991cd33cd3d38f33ff53313d
Dhananjoy <deydhananjoy5@gmail.com>
2023-03-25 22:51:18 +0530

    Added createBrowserRouter,  RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Home from './pages/Home';
 import Experience from './pages/Experience';
 import Projects from './pages/Projects';
 import Achievements from './pages/Achievements';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    // errorElement: <ErrorPage />,
+  },
+  {
+    path: "/Experience",
+    element: <Experience />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/achievements",
+    element: <Achievements />,
+  },
+]);
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-      <Route path='/' exact component={<Home/>} />
-      <Route path='/experience'  component={<Experience/>} />
-      <Route path='/projects'  component={<Projects/>} />
-      <Route path='/achievements'  component={<Achievements/>} />
-      </Routes>      
-    </Router>
-    
-    
+  return (  
+    <>
+      <Navbar />  
+      <RouterProvider router={router} />  
+    </>  
   );
 }
 
