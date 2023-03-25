32331d4d71a9a31a0a95286fb3e6a81dd36c46e0
Ashwini <akamzing412@gmail.com>
2023-03-25 23:21:33 +0530

    fixed-routing

--- src/index.js
+++ src/index.js
@@ -2,15 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import reportWebVitals from './reportWebVitals';
-
-
+import { createBrowserRouter, Outlet, RouterProvider, } from "react-router-dom";
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Achievements from './pages/Achievements';
+import Experience from './pages/Experience'
+import Navbar from './components/Navbar';
+import Footer from './pages/Footer';
+const AppLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout/>,
+    children: [
+      {
+        path: "/achievements",
+        element: <Achievements />,
+      },
+      {
+        path: "/experience",
+        element: <Experience />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />
+      }
+    ]
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />    
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
