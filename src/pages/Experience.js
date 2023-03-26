a680e57d8b6cc3ed72c525c52e318a16045d4922
Dhananjoy <deydhananjoy5@gmail.com>
2023-03-26 23:09:17 +0530

    Added experience, company, position, timeline, description divs

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,31 +3,25 @@ import './Experience.css'
 const Experiment = () =>{
     return(
         <div className="experience">
-            <div className="experience_1">
-                <h1>company name</h1><p>position</p>
+            <div className="experience 1">
+                <div className="company-name 1">
+
+                </div>
+                <div className="position 1">
+
+                </div>
+                <div className="timeline">
+
+                </div>
+                <div className="description">
+
+                </div>
+
+            </div>
+            <div className="demo-video">
                 
-                <ul>
-                    <p>Lorem</p>
-                    <p>Lorem</p>
-                    
-                    
-                </ul>
             </div>
-            <div className="experience_2">
-                <h1>company name | position</h1>
-                <ul>
-                    <li>lorem </li>
-                    
-                </ul>
-            </div>
-            <div className="experience_3">
-                <h1>company name | position</h1>
-                <ul>
-                    <li>lorem </li>
-                    <li>lorem </li>
-                    
-                </ul>
-            </div>
+            
         </div>
         )
 }
